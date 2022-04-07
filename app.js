c319904d0eca484b5851382b499ff540195d30ec
ThrinadhChowdary Gutta <98127965+thrinadh-chows@users.noreply.github.com>
2022-04-06 23:30:16 -0500

    created all things

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var giftRouter = require('./routes/gift')
+var addmodsRouter = require('./routes/addmods')
+var selectorRouter = require('./routes/selector')
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gift', giftRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
